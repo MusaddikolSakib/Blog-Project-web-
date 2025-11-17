0afcaa38f415b4f18dc6efa73eb8871d2132d84b
Musaddikol Sakib <musaddikulsakib111@gmail.com>
2025-11-16 22:04:02 -0800

    Update blog wesite with page and layout improvements

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 console.log("JavaScript is working!");
 alert("Welcome to The Daily Spark!");
 
+/*
 document.querySelectorAll('.blog-card button').forEach(button => {
   button.addEventListener('click', () => {
     alert("Feature coming soon! This will open the full post.");
   });
-});
+});*/
