8dc08c782c95ac2c6b9fe2e3d03b5784c6229d10
Musaddikol Sakib <musaddikulsakib111@gmail.com>
2025-11-18 21:00:20 -0800

    Modifyed by adding search bar and some fix

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@ document.querySelectorAll('.blog-card button').forEach(button => {
     alert("Feature coming soon! This will open the full post.");
   });
 });*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchInput");
+    const posts = document.querySelectorAll(".blog-card");
+
+    if (searchInput) {
+        searchInput.addEventListener("input", () => {
+            const value = searchInput.value.toLowerCase();
+
+            posts.forEach(post => {
+                const title = post.querySelector("h2").textContent.toLowerCase();
+
+                if (title.includes(value)) {
+                    post.style.display = "block";   // show
+                } else {
+                    post.style.display = "none";    // hide
+                }
+            });
+        });
+    }
+});
+
